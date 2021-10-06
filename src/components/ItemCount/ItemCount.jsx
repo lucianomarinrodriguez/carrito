dd3be749033951d60637a3af2b03484d1792ec49
Luciano Marin Rodriguez <lucianomarinrodriguez@gmail.com>
2021-10-06 15:31:15 -0300

    Desafio 5 - Se agrego el loader, el set time out, el map y se corrigio el contador de stock y cantidad

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -6,18 +6,27 @@ const ItemCount = ({ nombre, stock, img, addToCartWidget }) => {
   // La función addToCardWidget viene de ItemListContainer, suma la cantidad para agregar al carrito cuando apreto el botón, está declarada en Main
 
   // El state muestra la cantidad de cada Articulo que voy a agregar al carrito 
-  const [cantidad , setCantidad] = useState(1)
+  const [cantidad , setCantidad] = useState(0)
+
+
+    // El state se usa para aumentar o disminuir el stock cuando se aumenta la cantidad o disminuye la cantidad
+    const [newStock , setNewStock] = useState(stock)
 
 
   // Esta funcion solo vive en cada ItemCount y aumenta la cantidad de cada producto cuando tenga stock
   function add() {
-    if(cantidad < stock) setCantidad(cantidad+1)
-
+    if(cantidad < stock & newStock > 0) {
+      setCantidad(cantidad+1)
+      setNewStock(newStock-1)
+    }
   }
 
   // Esta funcion solo vive en cada ItemCount y resta la cantidad de cada producto hasta llegar a 1
   function remove() {
-    if( cantidad > 1) setCantidad(cantidad - 1)
+    if( cantidad > 0) {
+      setCantidad(cantidad - 1)
+      setNewStock(newStock+1)
+    }
   }
 
   return (
@@ -26,14 +35,16 @@ const ItemCount = ({ nombre, stock, img, addToCartWidget }) => {
           <img src={img} className="card-img-top h-100" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{nombre}</h5>
-            <p className="card-text">Stock : {stock}</p>
+            <p className="card-text">Stock : {newStock}</p>
             <div className="w-100 d-flex">
               <button onClick={()=>remove()} className="btn col-xs-6 btn-primary mx-auto">-</button>
               <span>Cantidad : {cantidad} </span>
               <button onClick={()=>add()} className="btn col-xs-6 btn-primary mx-auto">+</button>
             </div>
             <div className="row">
-              <button onClick={()=>addToCartWidget(cantidad)} className="btn btn-primary my-3">Agregar al Pedido</button>
+              <button className="btn btn-primary my-3" onClick={()=>{
+                addToCartWidget(cantidad);
+                setCantidad(cantidad-cantidad);}}>Agregar al Pedido</button>
             </div>
           </div>
         </div>
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,14 @@
 import React, { useState,useEffect } from 'react'
 import ItemListContainer from '../ItemListComtainer/ItemListContainer'
 import NavBar from '../NavBar/NavBar'
+import Loader from "react-loader-spinner"; 
 
 const Main = () => {
     
     const [carrito,setCarrito] = useState(0); 
     const [open,setOpen] = useState(true)
+    const [loading, setLoading] = useState(true)
+
 
     // Para mostrar y ocultar artículos, va a servir cuando quiera cargar el detalle del carrito
     
@@ -21,12 +24,30 @@ const Main = () => {
     // Effect que hace un console.log cuando agrego algo al carrito (se tiene que ir)
     useEffect(() => {
         console.log("se agrego un item")
+        setTimeout(() => {
+            setLoading(false)
+        }, 3000)
      }, [carrito])
 
     return (
         <div className="container-fluid p-2">
             <NavBar carrito={carrito} toggleMenu={toggleMenu} />
-            {open && <ItemListContainer greeting="Bienvenidos a Mi Tienda" addToCartWidget={addToCartWidget}/>}
+            <a>
+            {
+                loading
+                ?
+                <Loader
+                    className="p-5"
+                    type="Puff"
+                    color="#00BFFF"
+                    height={100}
+                    width={100}
+                    timeout={3000} //3 secs
+                />
+                :
+                open && <ItemListContainer greeting="Bienvenidos a Mi Tienda" addToCartWidget={addToCartWidget}/>
+            }
+            </a>
         </div>
     )
 }
